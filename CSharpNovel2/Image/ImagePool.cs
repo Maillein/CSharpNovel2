f6660ba01b85141bba954960acd52895d1054253
MShinya <realshinya1999@gmail.com>
2019-12-23 16:24:20 +0900

    ImagePool.csの軽微な修正

--- CSharpNovel2/Image/ImagePool.cs
+++ CSharpNovel2/Image/ImagePool.cs
@@ -1,18 +1,17 @@
 using System;
 using System.Collections.Generic;
-using System.Numerics;
 using SDL2;
 
 namespace CSharpNovel2.Image
 {
     public static class ImagePool
     {
-        private static Dictionary<string, int> _handleDictionary = new Dictionary<string, int>();
-        private static Dictionary<int, string> _nameDictionary = new Dictionary<int, string>();
-        private static Dictionary<int, Tuple<int, int>> _divImgWh = new Dictionary<int, Tuple<int, int>>();
-        private static Dictionary<int, Image> _images = new Dictionary<int, Image>();
-        private static Dictionary<int, int> _imagesCount = new Dictionary<int, int>();
-        private static List<int> UsedHandle = new List<int>();
+        private static readonly Dictionary<string, int> HandleDictionary = new Dictionary<string, int>();
+        private static readonly Dictionary<int, string> NameDictionary = new Dictionary<int, string>();
+        private static readonly Dictionary<int, Tuple<int, int>> DivImgWh = new Dictionary<int, Tuple<int, int>>();
+        private static readonly Dictionary<int, Image> Images = new Dictionary<int, Image>();
+        private static readonly Dictionary<int, int> ImagesCount = new Dictionary<int, int>();
+        private static readonly List<int> UsedHandle = new List<int>();
 
         private static int FindHandle()
         {
@@ -37,148 +36,148 @@ namespace CSharpNovel2.Image
         
         private static SDL.SDL_Rect GetSrcRect(int handle)
         {
-            return !_images.ContainsKey(handle) ? new SDL.SDL_Rect() : _images[handle].GetSrcRect();
+            return !Images.ContainsKey(handle) ? new SDL.SDL_Rect() : Images[handle].GetSrcRect();
         }
 
         public static int Load(string fileName)
         {
-            if (_handleDictionary.ContainsKey(fileName))
+            if (HandleDictionary.ContainsKey(fileName))
             {
-                _imagesCount[_handleDictionary[fileName]]++;
-                return _handleDictionary[fileName];
+                ImagesCount[HandleDictionary[fileName]]++;
+                return HandleDictionary[fileName];
             }
 
             var _nextHandle = FindHandle();
-            _images[_nextHandle] = new Image(fileName);
-            _imagesCount[_nextHandle] = 1;
-            _handleDictionary[fileName] = _nextHandle;
-            _nameDictionary[_nextHandle] = fileName;
+            Images[_nextHandle] = new Image(fileName);
+            ImagesCount[_nextHandle] = 1;
+            HandleDictionary[fileName] = _nextHandle;
+            NameDictionary[_nextHandle] = fileName;
             return _nextHandle;
         }
 
         public static int LoadDiv(string fileName, int numOfW, int numOfH)
         {
-            if (_handleDictionary.ContainsKey(fileName))
+            if (HandleDictionary.ContainsKey(fileName))
             {
-                _imagesCount[_handleDictionary[fileName]]++;
-                return _handleDictionary[fileName];
+                ImagesCount[HandleDictionary[fileName]]++;
+                return HandleDictionary[fileName];
             }
             
             var _nextHandle = FindHandle();
-            _images[_nextHandle] = new Image(fileName);
-            _imagesCount[_nextHandle] = 1;
-            _divImgWh[_nextHandle] = new Tuple<int, int>(numOfW, numOfH);
-            _handleDictionary[fileName] = _nextHandle;
-            _nameDictionary[_nextHandle] = fileName;
+            Images[_nextHandle] = new Image(fileName);
+            ImagesCount[_nextHandle] = 1;
+            DivImgWh[_nextHandle] = new Tuple<int, int>(numOfW, numOfH);
+            HandleDictionary[fileName] = _nextHandle;
+            NameDictionary[_nextHandle] = fileName;
             return _nextHandle;
         }
 
         public static bool Render(int handle, int x, int y)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            if (_images[handle].Render(x, y) == 0) return true;
-            Console.Error.WriteLine($"Unable to render {_nameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
+            if (!Images.ContainsKey(handle)) return false;
+            if (Images[handle].Render(x, y) == 0) return true;
+            Console.Error.WriteLine($"Unable to render {NameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
             return false;
         }
 
         public static bool RenderClip(int handle, int x, int y, SDL.SDL_Rect srcrect)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            if (_images[handle].RenderClip(x, y, srcrect) == 0) return true;
-            Console.Error.WriteLine($"Unable to render {_nameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
+            if (!Images.ContainsKey(handle)) return false;
+            if (Images[handle].RenderClip(x, y, srcrect) == 0) return true;
+            Console.Error.WriteLine($"Unable to render {NameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
             return false;
         }
 
         public static bool RenderClip(int handle, SDL.SDL_Rect srcrect, SDL.SDL_Rect dstrect)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            if (_images[handle].RenderClip(srcrect, dstrect) == 0) return true;
-            Console.Error.WriteLine($"Unable to render {_nameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
+            if (!Images.ContainsKey(handle)) return false;
+            if (Images[handle].RenderClip(srcrect, dstrect) == 0) return true;
+            Console.Error.WriteLine($"Unable to render {NameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
             return false;
         }
 
         public static bool RenderMag(int handle, int x, int y, double magnification)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            if (_images[handle].RenderMag(x, y, magnification) == 0) return true;
-            Console.Error.WriteLine($"Unable to render {_nameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
+            if (!Images.ContainsKey(handle)) return false;
+            if (Images[handle].RenderMag(x, y, magnification) == 0) return true;
+            Console.Error.WriteLine($"Unable to render {NameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
             return false;
         }
 
         public static bool RenderDiv(int handle, int index, int x, int y)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            var num = _divImgWh[handle];
+            if (!Images.ContainsKey(handle)) return false;
+            var num = DivImgWh[handle];
             var size = GetSrcRect(handle);
             var w = size.w / num.Item1;
             var h = size.h / num.Item2;
-            if (_images[handle].RenderClip(x, y,
+            if (Images[handle].RenderClip(x, y,
                     new SDL.SDL_Rect {x = w * (index % num.Item1), y = h * (index / num.Item2), w = w, h = h}) ==
                 0) return true;
-            Console.Error.WriteLine($"Unable to render {_nameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
+            Console.Error.WriteLine($"Unable to render {NameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
             return false;
         }
 
         public static bool RenderRotate(int handle, double angle)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            if (_images[handle].RenderRotate(angle) == 0) return true;
-            Console.Error.WriteLine($"Unable to render {_nameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
+            if (!Images.ContainsKey(handle)) return false;
+            if (Images[handle].RenderRotate(angle) == 0) return true;
+            Console.Error.WriteLine($"Unable to render {NameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
             return false;
         }
 
         public static bool RenderClipRotate(int handle, int x, int y, SDL.SDL_Rect srcrect, double angle)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            if (_images[handle].RenderClipRotate(x, y, srcrect, angle) == 0) return true;
-            Console.Error.WriteLine($"Unable to render {_nameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
+            if (!Images.ContainsKey(handle)) return false;
+            if (Images[handle].RenderClipRotate(x, y, srcrect, angle) == 0) return true;
+            Console.Error.WriteLine($"Unable to render {NameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
             return false;
         }
 
         public static bool RenderClipRotate(int handle, SDL.SDL_Rect srcrect, SDL.SDL_Rect dstrect, double angle)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            if (_images[handle].RenderClipRotate(srcrect, dstrect, angle) == 0) return true;
-            Console.Error.WriteLine($"Unable to render {_nameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
+            if (!Images.ContainsKey(handle)) return false;
+            if (Images[handle].RenderClipRotate(srcrect, dstrect, angle) == 0) return true;
+            Console.Error.WriteLine($"Unable to render {NameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
             return false;
         }
 
         public static bool RenderMagRotate(int handle, int x, int y, double magnification, double angle)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            if (_images[handle].RenderMagRotate(x, y, magnification, angle) == 0) return true;
-            Console.Error.WriteLine($"Unable to render {_nameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
+            if (!Images.ContainsKey(handle)) return false;
+            if (Images[handle].RenderMagRotate(x, y, magnification, angle) == 0) return true;
+            Console.Error.WriteLine($"Unable to render {NameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
             return false;
         }
 
         public static bool RenderDivRotate(int handle, int index, int x, int y, double angle)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            var num = _divImgWh[handle];
+            if (!Images.ContainsKey(handle)) return false;
+            var num = DivImgWh[handle];
             var size = GetSrcRect(handle);
             var w = size.w / num.Item1;
             var h = size.h / num.Item2;
-            if (_images[handle].RenderClipRotate(x, y,
+            if (Images[handle].RenderClipRotate(x, y,
                     new SDL.SDL_Rect {x = w * (index % num.Item1), y = h * (index / num.Item2), w = w, h = h}, angle) ==
                 0) return true;
-            Console.Error.WriteLine($"Unable to render {_nameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
+            Console.Error.WriteLine($"Unable to render {NameDictionary[handle]}. SDL Error: {SDL.SDL_GetError()}");
             return false;
         }
 
         public static bool Free(int handle)
         {
-            if (!_images.ContainsKey(handle)) return false;
-            if (_imagesCount[handle] > 1)
+            if (!Images.ContainsKey(handle)) return false;
+            if (ImagesCount[handle] > 1)
             {
-                _imagesCount[handle]--;
+                ImagesCount[handle]--;
                 return true;
             }
 
-            _images.Remove(handle);
-            _imagesCount.Remove(handle);
-            _handleDictionary.Remove(_nameDictionary[handle]);
-            _nameDictionary.Remove(handle);
-            _divImgWh.Remove(handle);
+            Images.Remove(handle);
+            ImagesCount.Remove(handle);
+            HandleDictionary.Remove(NameDictionary[handle]);
+            NameDictionary.Remove(handle);
+            DivImgWh.Remove(handle);
             UsedHandle.Remove(handle);
             
             return true;
